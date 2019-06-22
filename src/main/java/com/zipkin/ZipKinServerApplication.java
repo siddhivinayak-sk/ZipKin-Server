55a61c2d5bc8b58e474ed8fd932b2db0e220a421
Sandeep Kumar <kumar-sand@HCL.COM>
2019-06-22 18:46:29 +0530

    my first commit

---
+++ src/main/java/com/zipkin/ZipKinServerApplication.java
@@ -0,0 +1,18 @@
+package com.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin2.server.internal.EnableZipkinServer;
+
+
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipKinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipKinServerApplication.class, args);
+	}
+
+}
